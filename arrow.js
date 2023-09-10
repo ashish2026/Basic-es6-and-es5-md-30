0ccff5a3630d07cfffe7b5f59d7dc2a0b4debd3d
adhikari2003 <ashish.adhikary.2003@gmail.com>
2023-09-11 00:05:41 +0600

    new

---
+++ arrow.js
@@ -0,0 +1,18 @@
+// function add(a, b) {
+//           const result = a + b;
+//           return result;
+// }
+
+// function expression
+function add(a, b) {
+          return a + b;
+}
+
+// arrow function 
+const add3 = (a, b) => a + b;
+const add5 = (a, b) => a * b;
+
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+const sum = add4(5, 93, 4, 34)
+console.log(sum)
